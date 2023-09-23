0b7da91ca76f44ecfdeb994b70200178d585e3cc
Suleiman <suleosman73@gmail.com>
2023-09-23 11:22:54 +0300

    Done the speed-detector

---
+++ speed-detector.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')();
+
+let speedLimit = 70;
+let demeritPoints = 0;
+let outPut;
+
+// Asking for input
+const speedInputStr = prompt('Enter your speed: ');
+const speedInput = parseInt(speedInputStr, 10);
+let speedOverLimit = speedInput - speedLimit;
+
+//speed below limit
+if (speedInput < speedLimit) {
+    outPut = "OK"
+} 
+// Suspend the license
+else if (speedOverLimit >= 12) {
+    outPut = "License suspended"
+} 
+// calculating the points
+else {
+    demeritPoints = Math.floor(speedOverLimit / 5);
+    outPut = `Points Deducted: ${demeritPoints}`
+}
+
+console.log(outPut);
