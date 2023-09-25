8cac7c4abb7ea9701558a747ec24421b94a7f6ef
Suleiman <suleosman73@gmail.com>
2023-09-25 13:58:07 +0300

    Optimized

--- student-grade-generator.js
+++ student-grade-generator.js
@@ -10,21 +10,24 @@ const grade = parseInt(gradeStr, 10);
 let yourGrade;
 
 //finding the grades
-if(grade > 79){
-    yourGrade = "A";
+function studentGrading(grade){
+    if(grade > 79){
+        yourGrade = "A";
+    }
+    else if(grade >= 60 && grade <= 79){
+        yourGrade = "B";
+    }
+    else if(grade >= 49 && grade <=59){
+        yourGrade = "C";
+    }
+    else if(grade >= 40 && grade <= 49){
+        yourGrade = "C";
+    }
+    else{
+        yourGrade = "D";
+    }
+    return yourGrade
 }
-else if(grade >= 60 && grade <= 79){
-    yourGrade = "B";
-}
-else if(grade >= 49 && grade <=59){
-    yourGrade = "C";
-}
-else if(grade >= 40 && grade <= 49){
-    yourGrade = "C";
-}
-else{
-    yourGrade = "D";
-}
-   
+const graded = studentGrading(grade)
 //printing out the users grade
-console.log(`Your grade is: ${yourGrade}`)
+console.log(`Your grade is: ${graded}`)
